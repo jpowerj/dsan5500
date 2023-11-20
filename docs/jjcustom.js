e7c09bc7a5219cf6f293c063f32a368092e867bd
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2023-11-19 21:21:14 -0500

    Sidebar header

---
+++ docs/jjcustom.js
@@ -0,0 +1,5 @@
+window.document.addEventListener("DOMContentLoaded", function (event) {
+    var $subtitleDiv = $("<br><span class='subtitle'>Mon, 12:30-3:00pm</span><br><span class='subtitle'>Car Barn 204</span><br><span class='subtitle'>Prof. Jeff Jacobs&nbsp;<a href='mailto:jj1088@georgetown.edu' target='_blank'><i class='bi bi-envelope-at ps-1 pe-1'></i></a></span><br><span class='subtitle'>Spring 2024, Georgetown</span>");
+    $('.sidebar-title').append($subtitleDiv);
+  });
+  
